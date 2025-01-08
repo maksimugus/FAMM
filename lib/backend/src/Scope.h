e3327a8b288ee66f9248933c8b6349f61e8117f3
FeToR1 <fedornigretuk@gmail.com>
2025-01-08 05:31:21 +0300

    fixed: pragma once

--- lib/backend/src/Scope.h
+++ lib/backend/src/Scope.h
@@ -1,6 +1,7 @@
-#include <unordered_map>
-#include <string>
+#pragma once
 #include <llvm/IR/Instructions.h>
+#include <string>
+#include <unordered_map>
 
 struct Scope {
     std::unordered_map<std::string, llvm::AllocaInst*> variables;
