f7c89a3df81b9d842ef0ae7442ec167bb116d2c4
FeToR1 <fedornigretuk@gmail.com>
2025-01-18 22:04:29 +0300

    fixed: Frames

--- lib/backend/src/interpreter/CustomInterpreter.h
+++ lib/backend/src/interpreter/CustomInterpreter.h
@@ -12,35 +12,60 @@ enum Instr {
     ADD, // pop a, pop b, push a + b
     SUB, // pop a, pop b, push a - b
     AND, // pop a, pop b, push a & b
-    OR, // pop a, pop b, push a | b
+    OR,  // pop a, pop b, push a | b
     NOT, // pop a, push !a
     PRINT, // pop one byte and write to stream
     LOAD, // pop a, push byte read from address a
     STORE, // pop a, pop b, write b to address a
     GOTO, // pop a, goto a
-    PUSH, // push next word,
-    FRAME_APPEND,
-    FRAME_EXIT
+    PUSH, // push next word
+
+    FRAME_APPEND, // write new frame
+    FRAME_POP,    // pop frame
+
+    CALL, // push address, push arguments, create frame, goto address
+    RET,  // pop frame, restore pc
+
+    IF_EQ, // pop a, pop b, create frame, if (a == b) goto next
+    IF_NE, // pop a, pop b, create frame, if (a != b) goto next
+    IF_LT, // pop a, pop b, create frame, if (a < b) goto next
+    IF_GT, // pop a, pop b, create frame, if (a > b) goto next
+    IF_LE, // pop a, pop b, create frame, if (a <= b) goto next
+    IF_GE, // pop a, pop b, create frame, if (a >= b) goto next
 };
 
 using Value = std::variant<int64_t, bool, float, std::vector<int64_t>, void*>;
 
 struct Frame {
-    std::map<std::string, Value> localVariables;
-    std::stack<Value> operandStack;
-
+    std::map<std::string, Value> localVariables; // Локальные переменные
+    std::stack<Value> operandStack;             // Стек операндов
+    Frame* parentFrame;                         // Указатель на родительский фрейм
 };
 
 class CustomInterpreter {
-    std::stack<Frame> frameStack;
-    std::vector<Instr> program;
-    size_t pc;
+    std::stack<Frame*> frameStack; // Стек фреймов
+    std::vector<Instr> program;    // Программа
+    size_t pc;                     // Счетчик команд
 
 public:
-    explicit CustomInterpreter(const std::vector<Instr>& prog) : program(prog), pc(0) {}
+    explicit CustomInterpreter(const std::vector<Instr>& prog) : program(prog), pc(0) {
+        // Создаем начальный фрейм
+        frameStack.push(new Frame{{}, {}, nullptr});
+    }
+    ~CustomInterpreter() {
+        while (!frameStack.empty()) {
+            delete frameStack.top();
+            frameStack.pop();
+        }
+    }
+
     void run();
 
 private:
+    void frame_push();
+    void frame_pop();
+    Frame* current_frame();
+
     void instr_add();
     void instr_sub();
     void instr_and();
@@ -51,5 +76,14 @@ private:
     void instr_store();
     void instr_goto();
     void instr_push();
-    void frame_append();
+    void instr_frame_append();
+    void instr_frame_pop();
+
+    // Условные переходы
+    void instr_if_eq();
+    void instr_if_ne();
+    void instr_if_lt();
+    void instr_if_gt();
+    void instr_if_le();
+    void instr_if_ge();
 };
